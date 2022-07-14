6267a5c362c96059bdd938570c0d47103396d72d
And4Web <anands2807@gmail.com>
2022-07-14 14:13:17 +0530

    project finished

--- project-3-picture-in-picture/script.js
+++ project-3-picture-in-picture/script.js
@@ -1,1 +1,22 @@
-console.log("hello...");
+let videoElement = document.getElementById("video");
+let buttonElement = document.getElementById("button");
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log("Error here: ", error);
+  }
+}
+
+buttonElement.addEventListener("click", async () => {
+  buttonElement.disabled = true;
+  await videoElement.requestPictureInPicture();
+  buttonElement.disabled = false;
+});
+
+selectMediaStream();
