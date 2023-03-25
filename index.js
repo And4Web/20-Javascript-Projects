6b08cbd608f15b96f54f4bc4f593e64fcf8b5a92
And4Web <anands2807@gmail.com>
2023-03-25 19:41:02 +0530

    new express server created

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const PORT = 5500;
+const app = express();
+
+
+app.get("/", (req, res)=>{
+  res.json("server is good. Store all your endpoints here in order to hide the keys and sensitive data from the frontend users")
+})
+
+app.listen(PORT, ()=>{
+  console.log(`server running on ${PORT}`)
+})
